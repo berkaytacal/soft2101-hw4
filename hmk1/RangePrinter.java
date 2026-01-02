207c8d6590e351817637c3e9ba7ecef71f0e0b18
berkaytacal <146483619+berkaytacal@users.noreply.github.com>
2026-01-02 18:35:54 +0300

    Update RangePrinter.java

---
+++ hmk1/RangePrinter.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class RangePrinter {
+    public static void printRange(int a, int b) {
+        int start = Math.min(a, b) + 1;
+        int end = Math.max(a, b);
+
+        for (int i = start; i < end; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        printRange(x, y);
+    }
+}
